43d9109cce46294f61cd1dde52d44062efeb6ad9
Rish-18 <girishparab11@gmail.com>
2025-01-03 15:13:02 +0530

    admin api url added

--- admin/src/api/api.tsx
+++ admin/src/api/api.tsx
@@ -4,7 +4,7 @@ const BASE_URL = "http://localhost:3000";
 
 // Define API endpoints for admin
 const AdminBanners = `${BASE_URL}/api/admin/banner`;
-const AdminUploadBanners = `${BASE_URL}api/admin/upload-banner`;
+const AdminUploadBanners = `${BASE_URL}/api/admin/upload-banner`;
 const AdminTruckModels = `${BASE_URL}/api/admin/trucks`;
 const AdminAboutContent = `${BASE_URL}/api/admin/about`;
 const Enquiries = `${BASE_URL}/api/enquiries`;
