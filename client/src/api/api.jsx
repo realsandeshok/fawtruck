682a6abfbac07ae7e6f7e9d796d7d97c4f701e45
Rish-18 <girishparab11@gmail.com>
2025-01-07 18:08:17 +0530

    client api

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://fawtruck.onrender.com";
 
 const Banners = `${BASE_URL}/api/banner`;
 const TruckModel = `${BASE_URL}/api/trucks`;
